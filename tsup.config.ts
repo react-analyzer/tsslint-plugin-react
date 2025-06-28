7fd2c52de20627eb77a89ac194b36a7f7e3b4c43
Rel1cx <dokimondex@gmail.com>
2025-06-28 12:47:00 +0800

    fix: update external dependencies in tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default {
   clean: true,
   dts: true,
   entry: ["src/index.ts"],
-  external: ["eslint", "typescript"],
+  external: ["tsl", "typescript"],
   format: ["esm"],
   minify: false,
   outDir: "dist",
