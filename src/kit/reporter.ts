0d28d77978b999df8fb4b8e2bf3e86c3bab0edd5
Rel1cx <dokimondex@gmail.com>
2025-06-30 08:14:28 +0800

    refactor: minor improvements

--- src/kit/reporter.ts
+++ src/kit/reporter.ts
@@ -2,29 +2,35 @@ import type { Context, ReportDescriptor } from "tsl";
 import { dual, type unit } from "../lib/eff.ts";
 
 export interface Reporter<Ctx extends Context = Context> {
-  send: (descriptor: unit | null | ReportDescriptor) => void;
-  sendOrElse: <TElse>(descriptor: unit | null | ReportDescriptor, cb: () => TElse) => unit | TElse;
+  send: (descriptor: unit | ReportDescriptor) => void;
+  sendOrElse: <TElse>(descriptor: unit | ReportDescriptor, cb: () => TElse) => unit | TElse;
 }
 
 export const send: {
-  <Ctx extends Context = Context>(context: Ctx, descriptor: unit | null | ReportDescriptor): void;
-  <Ctx extends Context = Context>(context: Ctx): (descriptor: unit | null | ReportDescriptor) => void;
-} = dual(2, <Ctx extends Context = Context>(context: Ctx, descriptor: unit | null | ReportDescriptor) => {
+  <TCtx extends Context = Context>(context: TCtx, descriptor: unit | ReportDescriptor): void;
+  <TCtx extends Context = Context>(context: TCtx): (descriptor: unit | ReportDescriptor) => void;
+} = dual(2, <TCtx extends Context = Context>(context: TCtx, descriptor: unit | ReportDescriptor) => {
   if (descriptor == null) return;
   return context.report(descriptor);
 });
 
 export const sendOrElse: {
+  <TCtx extends Context, TElse>(context: Context, descriptor: unit | ReportDescriptor, cb: () => TElse): unit | TElse;
   // dprint-ignore
-  <Ctx extends Context, TElse>(context: Context, descriptor: unit | null | ReportDescriptor, cb: () => TElse): unit| TElse;
-  // dprint-ignore
-  <Ctx extends Context, TElse>(context: Context): (descriptor: unit | null | ReportDescriptor) => (cb: () => TElse) => unit | TElse;
-} = dual(3, (context: Context, descriptor: unit | null | ReportDescriptor, cb: () => unknown) => {
-  if (descriptor == null) return cb();
-  return context.report(descriptor);
-});
+  <TCtx extends Context, TElse>(context: Context): (descriptor: unit | ReportDescriptor) => (cb: () => TElse) => unit | TElse;
+} = dual(
+  3,
+  (
+    context: Context,
+    descriptor: unit | ReportDescriptor,
+    cb: () => unknown,
+  ) =>
+    descriptor == null
+      ? cb()
+      : context.report(descriptor),
+);
 
-export function make<Ctx extends Context = Context>(context: Context): Reporter<Ctx> {
+export function make<TCtx extends Context = Context>(context: Context): Reporter<TCtx> {
   return {
     send: (...args) => send(context, ...args),
     sendOrElse: (...args) => sendOrElse(context, ...args),
